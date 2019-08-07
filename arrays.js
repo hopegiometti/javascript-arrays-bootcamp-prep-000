c86454a09294f49ad1eabf551c1a814bfe51bcf5
Hope Giometti <hopegiometti@gmail.com>
2019-08-07 18:58:39 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,20 @@
+//my arrays!
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(chocolateBars, candyString) {
+  return [candyString, ...chocolateBars]
+}
+
+function destructivelyAddElementToBeginningOfArray(chocolateBars, candyString) {
+  chocolateBars.unshift(candyString)
+  return chocolateBars
+}
+
+function addElementToEndOfArray(chocolateBars, candyString) {
+  return [...chocolateBars, candyString]
+}
+
+function destructivelyAddElementToEndOfArray(chocolateBars, candyString) {
+  chocolateBars.push(candyString)
+  return chocolateBars
+}
